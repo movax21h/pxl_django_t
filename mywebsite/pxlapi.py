fd60cb96842609a47ba511572d92a945921256dc
xiaolong <254685286@qq.com>
2019-07-02 17:48:24 +0800

    getip_done

--- mywebsite/pxlapi.py
+++ mywebsite/pxlapi.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
  
 def clientip(request):
 	context = {}
-	context['clientip'] = request.GET
+	try:	
+		if 'HTTP_X_FORWARDED_FOR' in request.META:
+			context['clientip'] = request.META.get('HTTP_X_FORWARDED_FOR')
+		else:
+			context['clientip'] = request.META.get('REMOTE_ADDR')
+	except:
+		context['clientip'] = 'NONE'
+	
 	return render(request,'clientip.html',context)
