a8c3ab0b3f8b7a1e75d22c087b5258edf549ddad
xiaolong <254685286@qq.com>
2019-07-02 13:49:07 +0800

    getip0.19

--- mywebsite/pxlapi.py
+++ mywebsite/pxlapi.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
  
 def clientip(request):
 	context = []
-	context['clientip'] = dir(request)
+	context['clientip'] = request
 	return HttpResponse(request,'clientip.html',context)
