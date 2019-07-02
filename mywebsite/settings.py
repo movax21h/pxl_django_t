c248450bd5089e58ec5f633e49180e95e0527791
xiaolong <254685286@qq.com>
2019-07-02 14:25:10 +0800

    getip0.28

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH,"templates"),],
+        'DIRS': [os.path.join(SETTINGS_PATH,"mywebsite/templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
