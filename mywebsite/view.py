77b5d5be7944754a9d882120f5ad2753ef79196a
xiaolong <254685286@qq.com>
2019-07-03 11:31:13 +0800

    change404_0.2

--- mywebsite/view.py
+++ mywebsite/view.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
  
 def hello(request):
     return HttpResponse("Hello world ! ")
-def page_not_found(request,**kwargs):
-	html = "<html><h1>你要的东西并不存在<h1></html>"
+def page_not_found(request):
+	html = u"<html><h1>你要的东西并不存在<h1></html>".encode()
 	return HttpResponse(html)
