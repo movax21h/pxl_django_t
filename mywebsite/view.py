8ff10555a9865856a0a62f0b75f2335ac7e3567d
xiaolong <254685286@qq.com>
2019-08-29 12:27:08 +0800

    error

--- mywebsite/view.py
+++ mywebsite/view.py
@@ -1,14 +1,14 @@
 #encoding:utf-8
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render,render_to_response
  
 def hello(request):
     return HttpResponse("Hello world ! ")
 def page_not_found(request,exception):
 	html = u"<html><h1>你要的东西并不存在<h1></html>".encode()
-	
-	return render_to_response('404.html')
+	data = '你要的东西并不存在'
+	return render_to_response('404.html',{'mydata':data})
 def page_error(request):
 	html = u"<html><h1>你要的东西并不存在<h1></html>".encode()
-	
-	return render_to_response('404.html')
+	data = '你要的东西并不存在'
+	return render(html)
